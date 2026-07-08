214e1cc83994caf667a4db23bb0ac1eeea64b0c7
Sami Jan Mohammad <samijanm409@gmail.com>
2026-07-09 04:25:28 +0430

    another example of arrow function

--- javaScript/arrowfunction.js
+++ javaScript/arrowfunction.js
@@ -27,3 +27,10 @@ const sayHello3 = (name) =>{
     console.log('Hello ' + name);
 }
 sayHello3("sami");
+
+
+
+
+// and one we have only one code and want to use return no need for writting see the example : 
+const addNumbers = (num1 , num2) => num+num2;
+addNumbers(2,3);
