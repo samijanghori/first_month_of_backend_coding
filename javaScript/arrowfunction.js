01a924ef39ae81b948d864b747edc961c8982190
Sami Jan Mohammad <samijanm409@gmail.com>
2026-07-09 04:21:57 +0430

    defining the arrowfunction in js

---
+++ javaScript/arrowfunction.js
@@ -0,0 +1,17 @@
+// before Arrow function
+function sayHello()
+{
+    console.log("hello world!");
+}
+
+sayHello();
+
+
+
+
+
+// use new way of functions -> arrow function
+
+const sayHello2 = () => {
+    console.log("hello from arrow function")
+}
