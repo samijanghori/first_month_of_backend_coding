947ba2049bac5e054462abbd9380e529e17be3a9
Sami Jan Mohammad <samijanm409@gmail.com>
2026-07-11 07:54:25 +0430

    spread vs rest in js

---
+++ javaScript/RestSpread.js
@@ -0,0 +1,7 @@
+//The Spread Operator expands (unpacks)     ->    the elements of an array or the properties of an object into individual elements.
+// Rest is used to collect multiple values into one variable. 
+
+
+//spread
+const numbers = [2,3,4];
+console.log(...numbers);// it is called spread
